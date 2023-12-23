1b90cf6cec6ab44f201a16d5f5ffd76df532cbb3
Lucas Madranges <lucas.madranges.pro@icloud.com>
2023-12-23 10:18:05 +0100

    Creating and adding the components for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-function App() {
+import Logo from "./Logo.jsx";
+import Form from "./Form.jsx";
+import PackingList from "./PackingList.jsx";
+import Stats from "./Stats.jsx";
+
+export default function App() {
     return (
         <>
+            <div className="app">
+                <Logo/>
+                <Form/>
+                <PackingList/>
+                <Stats/>
+            </div>
         </>
     )
 }
-
-export default App
